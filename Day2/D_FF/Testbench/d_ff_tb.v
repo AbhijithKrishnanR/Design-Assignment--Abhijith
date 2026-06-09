8929225d2e1bdebe8a1ac0e8251da2ee0ce17439
AbhijithKrishnanR <24ec356@mgits.ac.in>
2026-06-09 21:33:28 +0530

    Update d_ff_tb.v

--- Day2/D_FF/Testbench/d_ff_tb.v
+++ Day2/D_FF/Testbench/d_ff_tb.v
@@ -1,1 +1,19 @@
-
+module d_ff_tb;
+    reg d,clk;
+    wire q,qbar;
+    d_ff df(q,qbar,d,clk);
+    initial
+    begin
+    clk=1'b0;
+    forever #5 clk=~clk;
+    end
+    initial
+    begin
+    d=1'b0; #10;
+    d=1'b1; #10;
+    d=1'b0; #10;
+    d=1'b1; #10;
+    d=1'b0; #10;
+    $stop;
+    end
+endmodule
