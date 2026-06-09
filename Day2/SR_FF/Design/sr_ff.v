91030899aa7da4d5c337f9a660a6f0f3f5526128
AbhijithKrishnanR <24ec356@mgits.ac.in>
2026-06-09 21:27:37 +0530

    Update sr_ff.v

--- Day2/SR_FF/Design/sr_ff.v
+++ Day2/SR_FF/Design/sr_ff.v
@@ -1,1 +1,24 @@
-
+module sr_flipflop(q,qbar,s,r,clk);
+    input s,r,clk;
+    output reg q;
+    output qbar;
+    assign qbar=~q;
+    always@(posedge clk)begin
+    if(s==1'b0&&r==1'b0)
+    begin
+    q<=q;
+    end
+    else if(s==1'b0 && r==1'b1)
+    begin
+    q<=1'b0;
+    end
+    else if(s==1'b1&&r==1'b0)
+    begin
+    q<=1'b1;
+    end
+    else
+    begin
+    q<=1'bx;
+    end
+    end
+endmodule
