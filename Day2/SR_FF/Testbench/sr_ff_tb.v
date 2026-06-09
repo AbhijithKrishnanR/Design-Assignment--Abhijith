2dc6677460b2c9f8bb40e030eea9418244799f7c
AbhijithKrishnanR <24ec356@mgits.ac.in>
2026-06-09 21:30:37 +0530

    Update sr_ff_tb.v

--- Day2/SR_FF/Testbench/sr_ff_tb.v
+++ Day2/SR_FF/Testbench/sr_ff_tb.v
@@ -1,1 +1,20 @@
+module sr_flipflop_tb;
+    reg s,r,clk;
+    wire q,qbar;
+    sr_flipflop sf(q,qbar,s,r,clk);
+        initial
+        begin
+        clk=0;
+        forever #5 clk=~clk;
+        end
+        initial
+        begin 
+        s=1'b0;r=1'b0; #10;
+        s=1'b1;r=1'b0; #10;
+        s=1'b0;r=1'b1; #10;
+        s=1'b1;r=1'b1; #10;
+        s=1'b0;r=1'b0; #10;
+        $stop;
+        end
+        endmodule
 
