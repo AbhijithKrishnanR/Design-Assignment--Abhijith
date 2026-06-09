b3b63557a4c404b9f6850d522f866698cc5a01e7
AbhijithKrishnanR <24ec356@mgits.ac.in>
2026-06-09 21:52:10 +0530

    Update Encoder4to2_tb.v

--- Day2/4_2 Encoder/Testbench/Encoder4to2_tb.v
+++ Day2/4_2 Encoder/Testbench/Encoder4to2_tb.v
@@ -1,1 +1,22 @@
+module encoder_4x2_tb;
+reg [3:0] I;
+wire [1:0] Y;
 
+encoder_4x2 uut (
+    .I(I),
+    .Y(Y)
+);
+initial
+begin
+    I = 4'b0001; #10;
+    I = 4'b0010; #10;
+    I = 4'b0100; #10;
+    I = 4'b1000; #10;
+    #10;
+    $finish;
+end
+initial
+begin
+    $monitor("Time=%0t I=%b Y=%b", $time, I, Y);
+end
+endmodule
