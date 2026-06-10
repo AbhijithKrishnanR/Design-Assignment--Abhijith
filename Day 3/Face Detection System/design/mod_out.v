154612120516a9267f19ab8d90930838b642a877
AbhijithKrishnanR <24ec356@mgits.ac.in>
2026-06-10 22:22:32 +0530

    Update mod_out.v

--- Day 3/Face Detection System/design/mod_out.v
+++ Day 3/Face Detection System/design/mod_out.v
@@ -1,1 +1,30 @@
+module mod_out(
+    input clk,
+    input rst,
+    input [7:0] d_in,
+    output reg [7:0] d_out
+);
 
+reg [1:0] state;
+
+parameter S0 = 2'd0,
+          S1 = 2'd1,
+          S2 = 2'd2;
+
+always @(posedge clk or posedge rst)
+begin
+    if (rst)
+        state <= S0;
+    else
+        state <= state + 1;  // cycles 0→1→2
+end
+
+always @(posedge clk or posedge rst)
+begin
+    if (rst)
+        d_out <= 0;
+    else if (state == S2)
+        d_out <= d_in;   // output only at 3rd clock
+end
+
+endmodule
