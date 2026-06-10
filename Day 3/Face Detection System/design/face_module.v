da69e0aa501ee194052277ab6485dd7e633832d9
AbhijithKrishnanR <24ec356@mgits.ac.in>
2026-06-10 22:19:32 +0530

    Update face_module.v

--- Day 3/Face Detection System/design/face_module.v
+++ Day 3/Face Detection System/design/face_module.v
@@ -1,1 +1,16 @@
+module face_mod(
+    input clk,
+    input rst,
+    input [7:0] s_in,
+    output reg [7:0] s_out
+);
 
+always @(posedge clk or posedge rst)
+begin
+    if (rst)
+        s_out <= 0;
+    else
+        s_out <= s_in;   // you can replace with real logic
+end
+
+endmodule
