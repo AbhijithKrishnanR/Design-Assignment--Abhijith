d8d2f9434c0b220ee82c27cee9c4bf15141d15b1
AbhijithKrishnanR <24ec356@mgits.ac.in>
2026-06-11 22:43:26 +0530

    Update block_ram_8x8.v

--- Day-4/Block Memory Generator/Design /block_ram_8x8.v
+++ Day-4/Block Memory Generator/Design /block_ram_8x8.v
@@ -1,67 +1,35 @@
 `timescale 1ns / 1ps
 
-module tb_block_ram_8x8;
+module block_ram_8x8 (
+    input  clk, 
+    input  arstn, 
+    input  wr_enb, 
+    input  [2:0] wr_address,   // 3-bit for 8 locations
+    input  [2:0] rd_address, 
+    input  [7:0] data_in, 
+    output reg [7:0] data_out 
+);
 
-    reg clk;
-    reg arstn;
-    reg wr_enb;
-    reg [2:0] wr_address;
-    reg [2:0] rd_address;
-    reg [7:0] data_in;
-    wire [7:0] data_out;
+    // 8 locations, each 8-bit
+    reg [7:0] ram [0:7];
+    integer i;
 
-    block_ram_8x8 uut (
-        .clk(clk),
-        .arstn(arstn),
-        .wr_enb(wr_enb),
-        .wr_address(wr_address),
-        .rd_address(rd_address),
-        .data_in(data_in),
-        .data_out(data_out)
-    );
+    always @(posedge clk or negedge arstn) begin
+        if (!arstn) begin
+            data_out <= 8'b0;
 
-    // clock generation
-    always #5 clk = ~clk;
+            // reset memory
+            for (i = 0; i < 8; i = i + 1)
+                ram[i] <= 8'b0;
+        end 
+        else begin
+            // write
+            if (wr_enb)
+                ram[wr_address] <= data_in;
 
-    initial begin
-        clk = 0;
-        arstn = 1;
-        wr_enb = 0;
-        wr_address = 0;
-        rd_address = 0;
-        data_in = 0;
-
-        // reset
-        #2 arstn = 0;
-        #15 arstn = 1;
-
-        // WRITE
-        @(posedge clk);
-        wr_enb = 1; wr_address = 3'd1; data_in = 8'hAA;
-
-        @(posedge clk);
-        wr_address = 3'd2; data_in = 8'hBB;
-
-        @(posedge clk);
-        wr_address = 3'd3; data_in = 8'h0C;
-
-        // READ
-        @(posedge clk);
-        wr_enb = 0;
-
-        rd_address = 3'd1;
-        @(posedge clk);
-
-        rd_address = 3'd2;
-        @(posedge clk);
-
-        rd_address = 3'd3;
-        @(posedge clk);
-
-        rd_address = 3'd5; // not written → expect 0
-        @(posedge clk);
-
-        #10 $stop;
+            // read (independent)
+            data_out <= ram[rd_address];
+        end
     end
 
 endmodule
